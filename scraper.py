6e98c52f6f2f58571e1f3d767bfec0b41b97a444
Aaron Levy <aaroncylevy@gmail.com>
2026-06-17 20:38:31 -0500

    Completed OverTheCap contract scraper - 2916 rows imported into contracts dataframe

--- scraper.py
+++ scraper.py
@@ -1,6 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
 
+from sqlalchemy import create_engine
+from dotenv import load_dotenv
+import os
 
 def scrape_overthecap():
     response = requests.get('https://overthecap.com/contracts')
@@ -10,7 +14,42 @@ def scrape_overthecap():
     tbody = table.find('tbody')
     rows = tbody.find_all('tr')
     
-    print(f"Found {len(rows)} rows")
-    print(rows[0])
+    data = []
+    
+    
+    for row in rows:
+        cols = row.find_all('td')
+        if len(cols) < 6:
+            continue
+        cols = row.find_all('td')
+        name = cols[0].text
+        position = cols[1].text
+        team = cols[2].text
+        total_value = cols[3].text
+        aav = cols[4].text
+        guaranteed  = cols[5].text
+        
+        data.append({
+            'name': name,
+            'position': position,
+            'team': team,
+            'total_value': total_value,
+            'aav': aav,
+            'guaranteed_money': guaranteed
+        })
+        
+    df = pd.DataFrame(data)
+    
+    for col in ['total_value', 'aav', 'guaranteed_money']:
+        df[col] = df[col].str.replace('$', '', regex=False).str.replace(',', '', regex=False)
+        df[col] = pd.to_numeric(df[col], errors='coerce')
+    
+    print(df.head())
+    print(df.shape)
+    
+    CONNECTION_STRING = "postgresql://aaronlevy@localhost:5432/nfl_intel"
+    engine = create_engine(CONNECTION_STRING)
+    df.to_sql('contracts', engine, if_exists='replace', index=False)
+    print("Loaded to contracts table")
     
 scrape_overthecap()
